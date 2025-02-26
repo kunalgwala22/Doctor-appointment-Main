82f36ed55f753001175362d290a2a432a218d73f
kunalgwala22 <139032907+kunalgwala22@users.noreply.github.com>
2025-02-26 23:29:37 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -17,7 +17,7 @@ const port=process.env.PORT || 4000
 
 //middlewares
 app.use(express.json())
-const allowedOrigins=['https://doctor-appointment-main-gamma.vercel.app']
+const allowedOrigins=['https://doctor-appointment-main-gamma.vercel.app','https://doctor-appointment-main-1gdv.vercel.app']
 app.use(cors({origin:allowedOrigins ,credentials:true}));
 
 //api endpoints 
